6b6e1782279d2d034c4c0e3234f743e8c2b2e678
chuvalik <chuvalik.work@gmail.com>
2022-03-16 11:00:58 +0300

    di (module, app, manifest update)

---
+++ app/src/main/java/com/example/testapp/di/AuthModule.kt
@@ -0,0 +1,33 @@
+package com.example.testapp.di
+
+import com.example.testapp.data.remote.AuthApi
+import com.example.testapp.data.repository.AuthRepositoryImpl
+import com.example.testapp.domain.repository.AuthRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AuthModule {
+
+    @Provides
+    @Singleton
+    fun provideAuthApi(): AuthApi {
+        return Retrofit.Builder()
+            .baseUrl(AuthApi.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(AuthApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideAuthRepository(authApi: AuthApi): AuthRepository {
+        return AuthRepositoryImpl(authApi)
+    }
+}
